fab3582e91a68ee2d5bcea5b59b9e9893d06e7c2
unknown <emma.laikmaa@gmail.com>
2025-03-07 15:30:56 +0200

    maini algus

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        boolean jätka = true;
+        while (jätka) {
+            Scanner scanner = new Scanner(System.in);  // Loome Scanneri objekti
+            System.out.print("Sisesta mis teha tahad (nr)");
+            String sisend = scanner.nextLine();  // Loeme sisestatud rea
+            scanner.close();
+
+            if (sisend.equals("1")) {
+                smile :)
+
+        }
+
+        }
+
     }
-} lol
+}
